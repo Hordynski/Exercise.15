2596519d187866cfb2f04663f6810fcb18d56605
Zachary Hordynski <zach.hordynski@gmail.com>
2018-10-15 20:29:39 -0400

    More practice pushing to github.

---
+++ Exercise.15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise._15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello user! Please enter a number: ");
+            int userInput = Int16.Parse(Console.ReadLine());
+            int n = 1;
+            do
+            {
+                int o = n * n * n;
+                Console.WriteLine(o);
+                n++;
+            } while (n <= userInput);
+
+            Console.ReadKey();
+        }
+    }
+}
